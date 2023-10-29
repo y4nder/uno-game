070fc1ac75a8028d207c03a0465b561b9641f3c5
yander lubguban <lorenzolubguban@gmail.com>
2023-10-29 22:16:12 +0800

    maximum players 10

--- UnoTest.java
+++ UnoTest.java
@@ -2,6 +2,9 @@ import java.util.List;
 import Participants.*;
 public class UnoTest {
     public static void main(String[] args){
+
+        //max players 10
+        //mi players 2
         Player p1 = new Cpu("Russell");
         Player p2 = new Cpu("Bianca");
         Player p3 = new Cpu("Eren");
@@ -12,15 +15,9 @@ public class UnoTest {
         Player p8 = new Cpu("Herv");
         Player p9 = new Cpu("Mirai");
         Player p10 = new Cpu("Micah");
-        Player p11 = new Cpu("Eishi");
-        Player p12 = new Cpu("Effie");
-        Player p13 = new Cpu("Changi");
-        Player p14 = new Cpu("Kobe");
-        Player p15 = new Cpu("Maple");
-
 
         RotationSetup set = new RotationSetup();
-        set.setupPlayers(List.of(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15));
+        set.setupPlayers(List.of(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10));
 
         UnoGame unoGame = new UnoGame(set.getFirstPlayer());
         unoGame.testStart();
